9cbee6083e25a3e842dc80036fd704c1093f34b9
remi carette <carette.remi@gmail.com>
2020-06-09 09:51:21 +0200

    create model Restaurant

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# Avant d'executer mes seeds
+# je clean ma base de données
+
+puts '===== Destroy all restaurants'
+
+Restaurant.destroy_all
+
+puts 'Create restaurants ...'
+
+# Restaurant.create(name: 'La Famille', address: '4 rue de Lille à Lomme', rating: 4)
+# Restaurant.create(name: 'So Good', address: '2 rue de Lille à Lomme', rating: 3)
+# Restaurant.create(name: 'Pick it up', address: '1 rue de Lille à Lomme', rating: 2)
+
+100.times do
+  Restaurant.create!(name: Faker::Restaurant.name, address: Faker::Address.full_address, rating: rand(1..5))
+end
+
+puts "... #{Restaurant.count} restaurants created"
